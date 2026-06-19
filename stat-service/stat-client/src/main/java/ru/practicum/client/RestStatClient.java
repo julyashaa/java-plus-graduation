1c59aaec09af906f88071b7b81e0632b51092f18
Юлия Родионова <seysspovered@yandex.ru>
2026-06-19 18:56:35 +0300

    Add Eureka, Config Server and Gateway modules

--- stat-service/stat-client/src/main/java/ru/practicum/client/RestStatClient.java
+++ stat-service/stat-client/src/main/java/ru/practicum/client/RestStatClient.java
@@ -1,6 +1,7 @@
 package ru.practicum.client;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriBuilder;
 import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.dto.ViewStatsDto;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -20,14 +22,16 @@ import static ru.practicum.constants.DatePatternConstant.DATE_TIME_PATTERN;
 @RequiredArgsConstructor
 public class RestStatClient implements StatClient {
 
-    private final RestClient restClient;
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN);
+    private final RestClient.Builder restClientBuilder;
+    private final DiscoveryClient discoveryClient;
 
+    private static final String STATS_SERVER = "stats-server";
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN);
 
     @Override
     public void hit(EndpointHitDto endpointHitDto) {
         try {
-            restClient.post()
+            getRestClient().post()
                     .uri("/hit")
                     .body(endpointHitDto)
                     .retrieve()
@@ -40,7 +44,7 @@ public class RestStatClient implements StatClient {
     @Override
     public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
         try {
-            ResponseEntity<List<ViewStatsDto>> response = restClient.get()
+            ResponseEntity<List<ViewStatsDto>> response = getRestClient().get()
                     .uri(uriBuilder -> {
                         UriBuilder builder = uriBuilder.path("/stats")
                                 .queryParam("start", start.format(FORMATTER))
@@ -66,7 +70,7 @@ public class RestStatClient implements StatClient {
     @Override
     public List<ViewStatsDto> getAllStats(List<String> uris, Boolean unique) {
         try {
-            ResponseEntity<List<ViewStatsDto>> response = restClient.get()
+            ResponseEntity<List<ViewStatsDto>> response = getRestClient().get()
                     .uri(uriBuilder -> {
                         UriBuilder builder = uriBuilder.path("/stats/all");
 
@@ -85,5 +89,17 @@ public class RestStatClient implements StatClient {
         } catch (Exception e) {
             throw new RuntimeException("Ошибка получения всей статистики", e);
         }
+    }
+
+    private RestClient getRestClient() {
+        List<ServiceInstance> instances = discoveryClient.getInstances(STATS_SERVER);
+
+        if (instances.isEmpty()) {
+            throw new RuntimeException("Сервис статистики не найден в Eureka");
+        }
+
+        return restClientBuilder
+                .baseUrl(instances.getFirst().getUri().toString())
+                .build();
     }
 }
--- stat-service/stat-client/src/main/java/ru/practicum/client/config/StatClientConfig.java
+++ stat-service/stat-client/src/main/java/ru/practicum/client/config/StatClientConfig.java
@@ -1,6 +1,5 @@
 package ru.practicum.client.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -8,13 +7,8 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class StatClientConfig {
 
-    @Value("${client.url}")
-    private String statServerUrl;
-
     @Bean
-    public RestClient restClient() {
-        return RestClient.builder()
-                .baseUrl(statServerUrl)
-                .build();
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
     }
 }
